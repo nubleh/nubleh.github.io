bc1d39d6288089a62c4ee143c84957f72b9f61b8
Nubli <nubli.naim@gmail.com>
2018-03-22 13:51:56 +0900

    land

---
+++ land/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var vm = new Vue({
+  el: '#mount',
+  template: '#main-app',
+  data: {
+    land: null,
+    size: [5, 5]
+  },
+  mounted: function(){
+    this.gen();
+  },
+  watch: {
+    size: function(next, prev){
+      if (next[0] !== prev[0] || next[1] !== prev[1]) {
+        this.gen();
+      }
+    }
+  },
+  computed: {
+  },
+  methods: {
+    gen: function(){
+      this.land = [];
+      for(var x = 0; x < this.size[0]; x++){
+        this.land[x] = [];
+        for(var y = 0; y < this.size[1]; y++){
+          this.land[x][y] = 0;
+        }        
+      }
+    }
+  }
+});
