9ecdae6b2c7428b397ea4e13ae264b29ec8774f0
Nubli <nubli.naim@gmail.com>
2018-06-09 21:43:32 +0900

    gard

--- gard/main.js
+++ gard/main.js
@@ -3,20 +3,24 @@ const c=document.getElementById('main');
 const debug = document.getElementById('debug');
 c.width = c.clientWidth;
 c.height = c.clientHeight;
+c.onmousemove = handleCursor;
 const ctx=c.getContext('2d');
 
 let tile = null;
+let cursorImg = null;
 requestAnimationFrame(render);
 
 const land = [];
-const landW = 10;
-const landH = 7;
+const landW = 9;
+const landH = 9;
 for(let y = 0; y < landH; y++){
   land[y] = [];
   for(let x = 0; x < landW; x++){
     land[y][x] = 0;
   }
 }
+
+const cursor = [4, 1];
 
 let lastRender = 0;
 let fps = 0;
@@ -37,6 +41,15 @@ function render(){
     img.src = 'tile.png';
     return;
   }
+  if (!cursorImg) {
+    const img = document.createElement('img');
+    img.onload = function(e){
+      cursorImg = e.target;
+      requestAnimationFrame(render);
+    };
+    img.src = 'cursor.png';
+    return;
+  }
 
   for(let y = 0; y < land.length; y++){
     for(let x = 0; x < land[y].length; x++){
@@ -44,7 +57,9 @@ function render(){
     }
   }
   clearCanvas();
-  drawLand();
+  const center = getCenter();
+  drawLand(center);
+  drawCursor(center);
 
   requestAnimationFrame(render);
 }
@@ -53,12 +68,34 @@ function clearCanvas(){
   ctx.clearRect(0, 0, c.width, c.height);
 }
 
-function drawLand(){
-  const centerX = c.width / 2;
-  const centerY = c.height / 2;
+function drawCursor(center){
+  const centerX = center[0];
+  const centerY = center[1];
+
+  let x = cursor[0] - (cursor[0] % 16);
+  let y = cursor[1] - (cursor[1] % 8);
+  if(((x - centerX)/16 + (y - centerY)/8)%2 > 0){
+    y += 8;
+  }
+  ctx.drawImage(
+    cursorImg,
+    x,
+    y
+//    centerX + (cursor[0] * 16) - (cursor[1] * 16),
+//    centerY + (cursor[0] * 8) + (cursor[1] * 8)
+  );
+}
+
+function drawLand(center){
+  const centerX = center[0];
+  const centerY = center[1];
   for(let y = 0; y < land.length; y++){
     for(let x = 0; x < land[y].length; x++){
-      ctx.globalAlpha = 0.5 + ((y + 1) / land.length) * 0.25 + ((x + 1) / land[y].length) * 0.25;
+      if (cursor[0] === x && cursor[1] === y) {
+        //ctx.globalAlpha = 0.85;
+      } else {
+        ctx.globalAlpha = 1;
+      }
       ctx.drawImage(
         tile,
         centerX + (x * 16) - (y * 16),
@@ -66,5 +103,32 @@ function drawLand(){
       );
     }    
   }
-  ctx.globalAlpha = 1;
+}
+
+function getCenter(){
+  let centerX = c.width / 2;
+  let centerY = c.height / 2;
+
+  // tile sprite is 32x32
+  centerX -= 16;
+  centerY -= 16;
+
+  // center on width
+  centerX -= (landW / 2) * 16 - 8;
+  centerY -= (landW / 2) * 8 - 4;
+
+  // center on height
+  centerX += (landH / 2) * 16 - 8;
+  centerY -= (landH / 2) * 8 - 4;
+  return [centerX, centerY];
+}
+
+function handleCursor(e){
+  const x = e.layerX;
+  const y = e.layerY;
+  const center = getCenter();
+  const cx = center[0] + 16;
+  const cy = center[1] + 16;
+  cursor[0] = x - 16;
+  cursor[1] = y - 8;
 }
