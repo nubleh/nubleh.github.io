bb28e88a7df31b512668426d245a644f4ea5e8e1
Nubli <nubli.naim@gmail.com>
2019-07-24 19:39:13 +0900

    ffxiv/craft

---
+++ ffxiv/craft/precache-manifest.5a4e404f918bf7292a5fa794687b4fc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd3063997803fbe1efffcadc293cc17",
+    "url": "/ffxiv/craft/index.html"
+  },
+  {
+    "revision": "3cc501f3451251e09969",
+    "url": "/ffxiv/craft/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "12f1f186d4864d99fe7f",
+    "url": "/ffxiv/craft/static/js/2.04c03956.chunk.js"
+  },
+  {
+    "revision": "3cc501f3451251e09969",
+    "url": "/ffxiv/craft/static/js/main.d674d5cb.chunk.js"
+  },
+  {
+    "revision": "c87be3e32a29a1c0e5d5",
+    "url": "/ffxiv/craft/static/js/runtime~main.9046a4f0.js"
+  }
+]);
